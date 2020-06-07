f33e24be6869b38988513e8092e723087d9995b2
Thomas Shannon <thomasshannon1117@gmail.com>
2020-06-06 22:46:44 -0700

    - added db config - added mysql config

--- server/router/example.js
+++ server/router/example.js
@@ -1,9 +1,10 @@
 const express = require('express')
-
 const router = express.Router()
+const conn = require('../db.js')
 
+// conn.query(sql , [], (err, results, fields) => {})
 router.get('/', (req, res, next) => {
-  res.send('Hello world!')
+
 })
 
 module.exports = router
