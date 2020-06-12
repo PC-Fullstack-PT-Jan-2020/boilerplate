f9b5d718940568bb0a6343c56658476ccde328b5
Thomas Shannon <thomasshannon1117@gmail.com>
2020-06-12 13:12:06 -0700

    - added user seed and authentication routes on express backend

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const exampleRoutes = require('./router/example')
+const userRoutes = require('./router/user')
 const PORT = 3001
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/api', exampleRoutes)
+app.use('/api', userRoutes)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
