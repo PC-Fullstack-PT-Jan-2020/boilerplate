6380b73ebf470218e1521986c7d7460a807f854a
Thomas Shannon <thomasshannon1117@gmail.com>
2020-06-13 09:14:43 -0700

    added comment about default user attachment

--- server/middleware/attachUser.js
+++ server/middleware/attachUser.js
@@ -1,5 +1,9 @@
 const jwtWebToken = require('jsonwebtoken')
 
+// TODO: I don' think this is needed
+// JWT seems to attach user regardless
+// may override that default functionality and
+// explicitly control user attachment here...
 module.exports = function (req, res, next) {
   const authorizationHeader = req.headers.authorization
   if (authorizationHeader) {
